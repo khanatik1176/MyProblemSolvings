b98324ac21a5f67a2a37c61fba5a5f0b9cd1b5cb
Khan Atik Faisal <khanatik1176@gmail.com>
2024-11-02 17:26:51 +0600

    Bitwise-Done

---
+++ CodeForces/bitwise.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int n;
+    string total;
+    cin>>n;
+
+    for(int i=0; i<n; i++)
+    {
+        cin>>total[i];
+        total = total[i];
+
+
+    }
+
+    cout<<"Total input:"<<total;
+
+}
